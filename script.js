ad996895c04f93bd27de16fe2d7986da98b143f5
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2024-08-27 14:38:53 +0500

    Add dark Button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
+const body = document.body;
 
 function addTask(){
     if(inputBox.value === ''){
@@ -16,7 +17,6 @@ function addTask(){
     inputBox.value = '';
     savedData();
 }
-
 listContainer.addEventListener('click', e => { // Arrow ftn
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
@@ -30,8 +30,21 @@ listContainer.addEventListener('click', e => { // Arrow ftn
 function savedData(){
     localStorage.setItem('data', listContainer.innerHTML);
 }
-
 function showTask(){
     listContainer.innerHTML = localStorage.getItem('data')
 }
 showTask();
+
+function toggleDarkMode() {
+    body.classList.toggle('darkMode');
+    localStorage.setItem('modes', body.classList.contains('darkMode') ? 'dark' : 'light');
+}
+function showMode() {
+    const savedMode = localStorage.getItem('modes');
+    if (savedMode === 'dark') {
+        body.classList.add('darkMode');
+    } else {
+        body.classList.remove('darkMode');
+    }
+}
+showMode();
